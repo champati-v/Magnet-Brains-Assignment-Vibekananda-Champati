c9987231002a7e3c9663a094d0be29f47b4ca521
vibekananda Champati <vibekanandac15@gmail.com>
2025-10-02 23:47:29 +0530

    project init and login page design

---
+++ Client/src/App.jsx
@@ -0,0 +1,24 @@
+import Home from "./pages/Home/Home"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Login from "./pages/Login/Login"
+import Signup from "./pages/Signup/Signup"
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/signup" exact element={<Signup />} />
+    </Routes>
+  </Router>
+)
+
+const App = () => {
+  return (
+    <div>
+      {routes}
+    </div>
+  )
+}
+
+export default App
---
+++ Client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between px-6 py-2'>
+        <h1 className='text-2xl font-semibold'>Task Manager</h1>
+    </div>
+  )
+}
+
+export default Navbar
